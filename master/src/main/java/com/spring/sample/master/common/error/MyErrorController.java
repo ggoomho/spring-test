569817b93deec310dcea3d8d57c810d3354f13b4
HoyeolChoi <food030921@hanmail.net>
2022-07-04 18:11:35 +0900

    spring project code

---
+++ master/src/main/java/com/spring/sample/master/common/error/MyErrorController.java
@@ -0,0 +1,9 @@
+package com.spring.sample.master.common.error;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyErrorController implements ErrorController {
+    
+}
---
+++ master/src/main/java/com/spring/sample/master/configuration/SwaggerConfig.java
@@ -0,0 +1,5 @@
+package com.spring.sample.master.configuration;
+
+public class SwaggerConfig {
+    
+}
